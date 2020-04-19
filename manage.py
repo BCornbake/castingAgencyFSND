6b2a4b9888cde62419c16a0e1c06bbb4e2fdb8bd
BCornbake <binfeng1219@126.com>
2020-04-19 14:10:18 +0800

    modify manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app_create
+from app import create_app
 from models import db
 
-app = app_create()
+app = create_app()
 migrate = Migrate(app, db)
 manager = Manager(app)
 
